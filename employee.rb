6e6188a3b81bb4fb391d4d1e67b7716a8b72ce3a
JackMarx <seriousfools@gmail.com>
2019-01-09 10:37:02 -0600

    class complete

--- employee.rb
+++ employee.rb
@@ -1,26 +1,12 @@
-# employee_1 = ["Nick", "Cage", 70000, true]
-# employee_2 = ["Julia", "Andrews", 80000, true]
-
-# puts employee_1[0] + " " + employee_1[1] + " makes " + employee_1[2].to_s + " a year."
-# puts "#{ employee_2[0] } #{ employee_2[1] } makes #{ employee_2[2] } a year."
-
-# employee_1 = {:first_name => "Nick", :last_name => "Cage", :salary => 70000, :active => true}
-# employee_2 = {salary: 80000, first_name: "Julia", last_name: "Andrews", active: true}
-
-# puts "#{ employee_1[:first_name] } #{ employee_1[:last_name] } makes #{ employee_1[:salary] } a year."
-# puts "#{ employee_2[:first_name] } #{ employee_2[:last_name] } makes #{ employee_2[:salary] } a year."
-
-
-
-class Employee
+ Employee
   attr_reader :first_name, :last_name, :active
   attr_writer :active
 
-  def initialize(input_first_name, input_last_name, input_salary, input_active)
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary = input_salary
-    @active = input_active
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
   end
 
   def print_info
@@ -33,12 +19,22 @@ class Employee
 end
 
 
-employee_1 = Employee.new("Nick", "Cage", 70000, true)
-employee_2 = Employee.new("Julia", "Andrews", 80000, true)
+employee_1 = Employee.new(
+                          first_name: "Nick", 
+                          last_name: "Cage", 
+                          salary: 70000, 
+                          active: true
+                          )
 
-p employee_2.active
-employee_2.active = false
-p employee_2.active
+employee_2 = Employee.new(
+                          first_name: "Julia", 
+                          last_name: "Andrews", 
+                          salary: 80000, 
+                          active: true
+                          )
+
+employee_1.print_info
+employee_2.print_info
 
 
 
